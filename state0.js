970b472f5327448c40d1f164c1db769728538c77
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-17 21:21:34 -0700

    make some adjustments to make the code look better

--- state0.js
+++ state0.js
@@ -20,14 +20,14 @@ function addKeyCallback (key, fn, args) {
 }
 
 function addChangeStateEventListeners() {
-  addKeyCallback(Phaser.Keyboard.ZERO, changeState, 0);
-  addKeyCallback(Phaser.Keyboard.ONE, changeState, 1);
-  addKeyCallback(Phaser.Keyboard.TWO, changeState, 2);
+  addKeyCallback(Phaser.Keyboard.ZERO,  changeState, 0);
+  addKeyCallback(Phaser.Keyboard.ONE,   changeState, 1);
+  addKeyCallback(Phaser.Keyboard.TWO,   changeState, 2);
   addKeyCallback(Phaser.Keyboard.THREE, changeState, 3);
-  addKeyCallback(Phaser.Keyboard.FOUR, changeState, 4);
-  addKeyCallback(Phaser.Keyboard.FIVE, changeState, 5);
-  addKeyCallback(Phaser.Keyboard.SIX, changeState, 6);
+  addKeyCallback(Phaser.Keyboard.FOUR,  changeState, 4);
+  addKeyCallback(Phaser.Keyboard.FIVE,  changeState, 5);
+  addKeyCallback(Phaser.Keyboard.SIX,   changeState, 6);
   addKeyCallback(Phaser.Keyboard.SEVEN, changeState, 7);
   addKeyCallback(Phaser.Keyboard.EIGHT, changeState, 8);
-  addKeyCallback(Phaser.Keyboard.NINE, changeState, 9);
+  addKeyCallback(Phaser.Keyboard.NINE,  changeState, 9);
 }
